624b71f9a146a9abc3fd23341c68858a6ec95d82
Sistemas Distribuidos <ana_fernanda_25@hotmail.com>
2018-11-21 17:45:44 -0500

    Redis service test

--- ci_server/gm_analytics/handlers.py
+++ ci_server/gm_analytics/handlers.py
@@ -1,21 +1,39 @@
+import os
+import logging
+import docker
 import requests
 import json
-from fabric import Connection
-from flask import request
+from flask import Flask, request
 
-def get_pullrequest_packages():
-    post_json_data   = request.get_data()
-    string_json      = str(post_json_data, 'utf-8')
-    json_pullrequest = json.loads(string_json)
-    pullrequest_sha  = json_pullrequest["pull_request"]["head"]["sha"]
-    packages_url     = "https://raw.githubusercontent.com/anavalderrama25/sd2018b-exam1/"+pullrequest_sha+"packages.json"
-    response_packages_url = requests.get(packages_url)
-    packages_data    =  json.loads(response_packages_url.content)
-    packages_line = ""
-    for i in packages_data:
-        packages = i[package_commands]
-        for j in packages:
-            packages_line = packages_line + " " + j
-    connect         = Connection('vagrant@192.168.137.3').run('sudo yum install --downloadonly --downloaddir=/var/repo' + packages_line)
-    out = {'command_return': 'OK'}
+def develop_branch_merged():
+    result_swagger       = ""
+    json_data           = request.get_data()
+    string_json         = str(post_json_data, 'utf-8')
+    json_pullrequest    = json.loads(string_json)
+    pullrequest_merged  = jsonFile["pull_request"]["merged"]
+
+    if pullrequest_merged:
+        pullrequest_sha  = jsonFile["pull_request"]["head"]["sha"]
+        images_url     = "https://raw.githubusercontent.com/anavalderrama25/sd2018b-exam2/" + pullrequest_sha + "images.json"
+        response_images_url = requests.get(images_url)
+        images_data    =  json.loads(response_images_url.content)
+        for service in images_data:
+            service_name = service["service_name"]
+            image_version = service["version"]
+            image_type = service["type"]
+            if image_type == 'Docker':
+                image_url = "https://raw.githubusercontent.com/anavalderrama25/sd2018b-exam2/" + pullrequest_sha + "/" + service_name + "Dockerfile"
+                file_image = requests.get(image_url)
+                file = open("Dockerfile" , "w")
+                file.write(str(file_image.content, 'utf-8'))
+                file.close()
+                image_tag = "localhost:5000/" + service_name + ":" + image_version
+                client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+                client.images.build(path="./", tag=image_tag)
+                client.images.push(image_tag)
+                client.images.remove(image=image_tag, force=True)
+                result_swagger = image_tag + " - Image built -" + result_swagger
+        out = {'command_return' : result_swagger}
+    else:
+        out = {'command return':'Pullrequest has not been merged'}
     return out
